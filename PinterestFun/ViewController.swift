82f9886c18a477fb381f12e31b11cc5036d1eaf5
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-03-30 22:20:06 -0400

    setup IBoutlets and configured Datasource Extension within the viewcontroller. similar to table views

--- PinterestFun/ViewController.swift
+++ PinterestFun/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // MARK: OUTLETS
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,3 +24,17 @@ class ViewController: UIViewController {
 
 }
 
+// MARK: DATA SOURCE
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 10
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+        
+        return cell
+    }
+}
