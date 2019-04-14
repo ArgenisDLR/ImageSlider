90a0602896595794bf268dcc0c3a0f3989ffd8a4
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-04-13 20:32:15 -0400

    brought in Ray W photo layout delegate protocol and configured  it to the ViewController

--- PinterestFun/ViewController.swift
+++ PinterestFun/ViewController.swift
@@ -16,12 +16,26 @@ class ViewController: UIViewController {
     
     // MARK: Properties
     
-    let images = [photonog, image2, image3, image4, image5, image6, image7]
+    let images = [#imageLiteral(resourceName: "image-1"), #imageLiteral(resourceName: "image-3"), #imageLiteral(resourceName: "image-5"), #imageLiteral(resourceName: "image-6"), #imageLiteral(resourceName: "image-7"), #imageLiteral(resourceName: "image-2"), #imageLiteral(resourceName: "image-4")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        if let layout = collectionView.collectionViewLayout as? PinterestLayout {
+            layout.delegate = self
+        } 
+        collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
+}
+
+//MARK: - Flow layout delegate
+
+extension ViewController: PinterestLayoutDelegate {
+    func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat {
+        let image = images[indexPath.item]
+        let height = image.size.height
+        
+        return height
     }
 }
 
@@ -30,7 +44,7 @@ class ViewController: UIViewController {
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return 10
+        return images.count
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
